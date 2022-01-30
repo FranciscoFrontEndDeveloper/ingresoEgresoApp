d9da7d2f7bd27c76a0d441082c4403ada64460a0
Francisco <pachonetworking@gmail.com>
2022-01-29 19:15:38 -0500

    formulario de logueo creado

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.sass']
+  styleUrls: ['./register.component.sass'],
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  formLogin: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.formLogin = this.formBuilder.group({
+      nombre:   ['', Validators.required],
+      correo:   ['', Validators.required, Validators.email],
+      password: ['', Validators.required],
+    })
   }
 
+  crearUsuario(): void{
+    console.log(this.formLogin);
+  }
 }
