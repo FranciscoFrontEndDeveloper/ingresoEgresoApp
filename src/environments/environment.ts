29cabc8864e23a2daf1b6fa7073cdb63dd3b7bc6
Francisco <pachonetworking@gmail.com>
2022-01-29 21:07:53 -0500

    angularFire instalado de forma correcta

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ingresos-egresos-app-5d95a',
+    appId: '1:1003296956879:web:7107283baca00bdade72ba',
+    storageBucket: 'ingresos-egresos-app-5d95a.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDFfS8513tFWdrerhCtaBvYG7r_vyFFI5k',
+    authDomain: 'ingresos-egresos-app-5d95a.firebaseapp.com',
+    messagingSenderId: '1003296956879',
+    measurementId: 'G-CKZXBJHPMJ',
+  },
   production: false
 };
 
